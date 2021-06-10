7255f4dec2609dcc795bf77fd178a2a23b66f76e
Gessé Carlos <gesse.dev@outlook.com>
2021-06-10 11:10:15 -0300

    adicionei novo projeto

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-import FinishButton from "./components/finishTask.js";
-import DeleteButton from "./components/deleteTask.js";
+import {handleNovoItem} from './componentes/criaTarefa.js'
+import {carregaTarefa} from './componentes/carregaTarefa.js'
+const novaTarefa = document.querySelector('[data-form-button]')
 
-const formButton = document.querySelector("[data-form-button]");
-const taskSetter = (event) => {
-  event.preventDefault();
+novaTarefa.addEventListener('click', handleNovoItem)
 
-  const unlist = document.querySelector("[data-list]");
-  const formInput = document.querySelector("[data-form-input]");
-  const inputValue = formInput.value;
-  
-  const task = document.createElement("li");
-  task.classList.add("task");
-  const content = `<p class="content">${inputValue}</p>`;
-
-  task.innerHTML = content;
-
-  task.appendChild(FinishButton());
-  task.appendChild(DeleteButton());
-  unlist.appendChild(task); 
- 
-}
-
-formButton.addEventListener("click", taskSetter);
-
+carregaTarefa()
